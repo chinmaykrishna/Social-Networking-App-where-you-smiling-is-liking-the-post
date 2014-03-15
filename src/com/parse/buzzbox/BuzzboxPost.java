4882d339fa89ebbe20fafb2ce1e2988e94d6a73e
chinmaykrishna <seekay3@gmail.com>
2014-03-16 01:19:27 +0530

    class object

---
+++ src/com/parse/buzzbox/BuzzboxPost.java
@@ -0,0 +1,46 @@
+package com.parse.buzzbox;
+
+import com.parse.ParseClassName;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+
+	/**
+	 * Data model for a post.
+	 */
+	@ParseClassName("Posts")
+	public class BuzzboxPost extends ParseObject {
+	  public String getText() {
+	    return getString("text");
+	  }
+
+	  public void setText(String value) {
+	    put("text", value);
+	  }
+
+	  public ParseUser getUser() {
+	    return getParseUser("user");
+	  }
+
+	  public void setUser(ParseUser value) {
+	    put("user", value);
+	  }
+
+	  public ParseGeoPoint getLocation() {
+	    return getParseGeoPoint("location");
+	  }
+
+	  public void setLocation(ParseGeoPoint value) {
+	    put("location", value);
+	  }
+
+	  //to put likes, dislikes to post
+	  
+	  public static ParseQuery<BuzzboxPost> getQuery() {
+	    return ParseQuery.getQuery(BuzzboxPost.class);
+	  }
+	}
+
+
--- src/com/parse/buzzbox/MainActivity.java
+++ src/com/parse/buzzbox/MainActivity.java
@@ -1,5 +1,14 @@
 package com.parse.buzzbox;
 
-public class MainActivity {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
 
 }
